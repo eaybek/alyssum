b39fa43bf7a111eef24a4864ecd2bcf6256a2cdb
Erdem Aybek <eaybek@gmail.com>
2018-12-16 14:13:11 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Alyssum
+
+this package contains my personal helpers if you want to use below the usage examples
+
+```python
+from alyssum.seperators import Title
+
+title = Title(label='Default Text'length=80, color='yellow')
+title.configure(label='Default Text'length=80,color='blue')
+
+title.write() # writes "Default Text"
+title.write("any title") # writes "any title"
+
+title.configure(color=blue).write("any title") # can be usable
+```
+
+
+
+label length can not be greater than (length - 2)
+color can be 'grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan' or 'white'
+length must be greater than 1 and smaller than 81 
+
---
+++ alyssum/seperators.py
@@ -0,0 +1,69 @@
+from termcolor import colored
+
+
+class Title(object):
+    """
+    this class contains a helper function which you can configure every single instance differently
+    """
+
+    def __init__(self, label='', length=80, color='blue', design='#'):
+        self.configure(label, length, color)
+
+    def configure(self, label=None, length=None, color=None, design=None):
+        if length is not None:
+            if length <= 2 or length >= 80:
+                raise Exception("Length must greater than 2 and smaller than 80")
+            self.length = length
+        if label is not None:
+            if len(label) > self.length-2:
+                raise Exception("label can not be greater than (length - 2) ")
+            self.label = label
+        if color is not None:
+            if color not in ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']:
+                raise Exception("color can be 'grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan' or 'white'")
+            self.color = color
+        if design_char is not None:
+            if len(design_char) =! 1:
+                raise Exception("Design char length must be 1")
+            self.design_char = design_char
+        return self
+
+    def write(self, label=None):
+        """
+        helper
+        hashtag label function
+        this function is print label in center of print(length*"#")
+        label:  Message what you say
+        length: Column number
+
+        """
+        if label is not None:
+            self.configure(label=label)
+
+        if self.length < 2:
+            raise Exception("length must greater than 2")
+        len_of_label = len(self.label)
+        if len_of_label == 0:
+            left_space = self.length/2
+            rigth_space = self.length/2
+        elif len_of_label < 0 or len_of_label > self.length - 2:
+            raise Exception("Out of range")
+        elif len_of_label % 2 == 0:
+            left_space = (self.length - len_of_label) / 2
+            if self.length % 2 == 0:
+                rigth_space = left_space
+            else:
+                rigth_space = left_space+1
+        elif len_of_label % 2 == 1:
+            left_space = (self.length - len_of_label) / 2
+            if self.length % 2 == 0:
+                rigth_space = left_space+1
+            else:
+                rigth_space = left_space
+        print(colored(self.length*"#", self.color))
+        print(colored("#", self.color), end='')
+        print((int(left_space)-1)*" ", end='')
+        print(colored(label.upper(), self.color), end='')
+        print((int(rigth_space)-1)*" ", end='')
+        print(colored("#", self.color))
+        print(colored(length*"#", self.color))
