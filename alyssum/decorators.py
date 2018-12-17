46df3e7c3ddf29fadc427003da18cbf925a1a1f6
Erdem Aybek <eaybek@gmail.com>
2018-12-17 14:32:53 +0300

    add decorator message

--- alyssum/decorators.py
+++ alyssum/decorators.py
@@ -1,10 +1,20 @@
-def run_only(time):
+def run_only(time, message=None):
     def decorator(func):
         def wrapper(*args, **kwargs):
             if wrapper.count != time:
                 wrapper.count += 1
                 return func(*args, **kwargs)
+            else:
+                if message is not None:
+                    print(message)
         wrapper.count = 0
         return wrapper
     return decorator
-run_once = run_only(1)
+
+
+def run_once_message(message=None):
+    def decorator(func):
+        return run_only(1, message)(func)
+    return decorator
+
+run_once = run_once_message(None)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="alyssum",
-    version="0.0.9",
+    version="0.0.10",
     author="Erdem Aybek",
     author_email="eaybek@gmail.com",
     description="A small helper package for personal needs",
