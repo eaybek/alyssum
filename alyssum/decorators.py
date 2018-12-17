f58b34f5f035b3db5988f3b833ec0adb5bc4e0ad
Erdem Aybek <eaybek@gmail.com>
2018-12-17 16:42:02 +0300

    add decorator memory management

--- alyssum/decorators.py
+++ alyssum/decorators.py
@@ -1,13 +1,45 @@
+counter_list = []
+
+
+class Counter(object):
+    counter = 0
+
+    def __init__(self, time, func):
+        self.time = time
+        self.func = func
+
+    def count(self):
+        if self.counter != self.time:
+            self.counter += 1
+            return True
+        else:
+            return False
+
+
+def search_list(func):
+    for i in counter_list:
+        if i.func == func:
+            return i
+    return None
+
+
+def get_counter(func, time):
+    counter = search_list(func)
+    if counter is None:
+        counter = Counter(time, func)
+        counter_list.append(counter)
+    return counter
+
+
 def run_only(time=1, message=None):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            if wrapper.count != time:
-                wrapper.count += 1
+            c = get_counter(func.__name__, time)
+            if c.count():
                 return func(*args, **kwargs)
             else:
                 if message is not None:
                     print(message)
-        wrapper.count = 0
         return wrapper
     return decorator
 
